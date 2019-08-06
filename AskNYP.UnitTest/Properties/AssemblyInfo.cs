c9c5962b06364112d67d84ff4a7842bb42045236
junjie800 <35260042+junjie800@users.noreply.github.com>
2019-08-06 16:00:03 +0800

    Add files via upload

---
+++ AskNYP.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AskNYP.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("AskNYP.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("009cce4c-e4a2-44a7-b68c-4c08ff80766b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AskNYP.UnitTest/UnitTest1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using OfficeOpenXml;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        static void Main(string[] args)
+        {
+            UnitTest1 Testing = new UnitTest1();
+            Testing.TestMethod1();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            string questions;
+            string columnheader;
+            string newcolumnheaders;
+            string answercells;
+            string responsecells;
+            string fakeresponses;
+            int count = 0;
+            int yescount = 0;
+
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://asknypadmindev.azurewebsites.net/botmain");
+            driver.Manage().Window.Maximize();
+            IWebElement imageclick = driver.FindElement(By.XPath("//img[@src='https://asknypadmin.azurewebsites.net/BotFolder/NYPChatBotRight.png']"));
+            imageclick.Click();
+            IWebElement frame = driver.FindElement(By.XPath(".//iframe[@id='nypBot']"));
+            driver.SwitchTo().Frame(frame);
+            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[3]/div/input")).Click();
+
+            //create a list to hold all the values
+            List<string> excelData = new List<string>();
+            //read the Excel file as byte array
+            byte[] bin = File.ReadAllBytes(@"C:\Users\manyp\Desktop\JJ\Real JJ Project\Overall_QnA4.xlsx");
+            //create a new Excel package in a memorystream
+            using (MemoryStream stream = new MemoryStream(bin))
+            using (ExcelPackage excelPackage = new ExcelPackage(stream))
+            {
+                excelPackage.Workbook.Worksheets.Delete("Normalized Values");
+                excelPackage.Workbook.Worksheets.Delete("Subject Areas");
+                excelPackage.Workbook.Worksheets.Delete("EAE");
+                //loop all worksheets
+                for (int w = 1; w <= 3; w++)   //Sheets 1 2 3 are Subject Areas, Normalized Values, EAE which we dont use.
+                //foreach (ExcelWorksheet worksheet in excelPackage.Workbook.Worksheets.ToList())
+                {
+                    //Console.WriteLine("TEST 1 Pass");
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[w];
+
+                    //Delete normalized values sheet
+                    Console.WriteLine("TEST 2 Pass");
+
+                    //loop all rows
+                    for (int i = worksheet.Dimension.Start.Row; i <= worksheet.Dimension.End.Row; i++)
+                    {
+                        //loop all columns in a row
+                        for (int j = worksheet.Dimension.Start.Column; j <=
+                       worksheet.Dimension.End.Column; j++)
+                        {
+                            //add the cell data to the List
+                            if (worksheet.Cells[i, j].Value != null)
+                            {
+                                excelData.Add(worksheet.Cells[i, j].Value.ToString());
+                            }
+                            //worksheet.Cells[1, worksheet.Dimension.End.Column + 1].Value = "Response";  //may need to set as .Value.ToString()
+                        }
+                    }
+                    int colCount = worksheet.Dimension.End.Column;
+                    int rowCount = worksheet.Dimension.End.Row;
+                    worksheet.Cells[1, colCount + 1].Value = "Responses";
+                    worksheet.Cells[1, colCount + 2].Value = "Timing of response retrieval";
+                    worksheet.Cells[1, colCount + 3].Value = "Does the answer and response match?";
+                    Console.WriteLine("Rows Count: " + (rowCount - 1));
+
+                    for (int i = 2; i <= rowCount; i++)
+                    {
+                        Console.WriteLine("Worksheet name: " + worksheet);
+                        questions = worksheet.Cells[i, 1].Text;
+                        Console.WriteLine("Questions are:" + questions);
+                        driver.FindElement(By.XPath("//html/body/div[1]/div/div/div[3]/div/input")).SendKeys(questions); //"Send questions"
+                        driver.FindElement(By.XPath("//html/body/div[1]/div/div/div[3]/button[1]")).Click(); //click button to send the question
+                        Thread.Sleep(1000); //Code passed till here so far (Checkpoint 1) (tick)
+                        fakeresponses = worksheet.Cells[i, 2].Text;
+                        //worksheet.Cells[i, colCount + 1].Value = fakeresponses;
+
+                        count += 1;
+
+                        //TRY THIS CODE OUT
+                        // foreach (var textmsg in textboxmsg)
+                        // {
+                        for (int c = 1; c <= colCount; c++)
+                        {
+
+                            columnheader = worksheet.Cells[1, c].Text;
+                            newcolumnheaders = worksheet.Cells[1, c].Text;
+                            answercells = worksheet.Cells[i, 2].Text;
+                            responsecells = worksheet.Cells[i, 3].Text;
+
+                            Console.WriteLine("Column header: ", columnheader);
+                            Console.WriteLine("New Column Header: ", newcolumnheaders);
+
+                            //retrieve response with all tags then remove all the tags below
+                            //try
+                            //{
+
+                            //Console.WriteLine(newcolumnheaders);
+                            if (columnheader == "Question")
+                            {
+
+                            }
+                            else if (columnheader == "Answer")
+                            {
+
+                            }
+                            else if (columnheader == "Answers")
+                            {
+
+                            }
+                            else if (newcolumnheaders == "Responses")
+                            {
+                                //Console.WriteLine("YES " + count);
+                                try
+                                {
+
+                                    // NewWorkSheet.Cells[i, c] = outerhtml2;
+                                    worksheet.Cells[i, c].Value = fakeresponses;
+                                }
+                                catch
+                                {
+                                }
+                                //outerhtml.Contains((char)13);
+                                //Console.WriteLine(outerhtml.Contains((char)13));
+                                //Console.WriteLine("WELP:" + outerhtml);
+
+                            }
+                            else if (newcolumnheaders == "Timing of response retrieval")
+                            {
+                                try
+                                {
+                                    worksheet.Cells[i, c].Value = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss");
+                                }
+                                catch
+                                {
+                                }
+                            }
+                            else if (newcolumnheaders == "Does the answer and response match?")
+                            {
+                                if (answercells.Equals(responsecells))
+                                {
+                                    try
+                                    {
+                                        worksheet.Cells[i, c].Value = "Yes";
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                                else
+                                {
+                                    try
+                                    {
+                                        worksheet.Cells[i, c].Value = "No";
+                                    }
+                                    catch
+                                    {
+                                    }
+                                }
+                            }
+                            else if (columnheader == null || newcolumnheaders == null)
+                            {
+                                Console.WriteLine("Headers empty");
+                            }
+                            else
+                            {
+
+                                worksheet.DeleteColumn(c);
+                                c--;
+                                colCount = worksheet.Dimension.End.Column;
+                                Console.WriteLine("HEY MAN STOP IT");
+
+                            }
+                        }
+                        if (worksheet.Cells[i, 5].Text == "Yes")
+                        {
+                            yescount += 1;
+                            //Console.WriteLine("For yes:" + NewWorkSheet.Cells[1][i].Text);
+                            //Console.WriteLine("Yes: " + yescount);
+                        }
+                        else
+                        {
+
+                        }
+
+                    }
+                }
+                excelPackage.Workbook.Worksheets.Add("Summary Report");
+                excelPackage.Workbook.Worksheets.MoveToStart("Summary Report");
+                ExcelWorksheet SummaryReport = excelPackage.Workbook.Worksheets["Summary Report"];
+                SummaryReport.Cells[1, 1].Value = "Total Count of Matches: " + count;
+                SummaryReport.Cells[2, 1].Value = "Total Count of Matched matched: " + yescount;
+
+                excelPackage.SaveAs(new FileInfo(@"D:\New.xlsx"));
+            }
+
+        }
+
+    }
+}
